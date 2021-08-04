096fcabede8aa7ce2afed1c0fe99ac34722b24e1
Ilya201091 <ZiK201091@ya.ru>
2021-08-04 23:57:38 +0700

    Нет подключение к БД

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,34 +2,90 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
 
+
+    private Connection connection;
+    private Statement statement;
+    private PreparedStatement preparedStatement;
+    private final String DELETE = "DELETE FROM User WHERE id = ?";
+    private final String GETALL = "SELECT * FROM User";
+
+    public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-
+        try  {
+            statement.executeUpdate("INSERT INTO User " +
+                    "(id INTEGER not NULL," +
+                    "name VARCHAR(50)," +
+                    "lastName VARCHAR(50)," +
+                    "age INTEGER not NULL");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
+        try {
+            preparedStatement = connection.prepareStatement("DELETE FROM User WHERE NAME");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        try {
+            preparedStatement.setString(2,name);
+            preparedStatement.setString(3,lastName);
+            preparedStatement.setInt(4,age);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void removeUserById(long id) {
-
+        try {
+            preparedStatement = connection.prepareStatement(DELETE);
+            preparedStatement.setLong(1,id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
+        try {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            User user = new User();
+            List<User> users = new ArrayList<>();
+            while (resultSet.next()){
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                users.add(user);
+            }
+            return users;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     public void cleanUsersTable() {
+        try {
+            preparedStatement = connection.prepareStatement("DELETE FROM User");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,28 @@
 package jm.task.core.jdbc.util;
 
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
+    private static final String URL = "jdbc:mysql://localhost:3306/INTtech?serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Bkmz201091";
+    private Connection connection;
+
+    public Util(){
+        try {
+            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+
+
+
     // реализуйте настройку соеденения с БД
 }
